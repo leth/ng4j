cdf2d74972fa676bf0fc176db0d8e7b86987e5b9
cyganiak <cyganiak>
2004-11-22 00:46:18 +0000

    Classes for TriG parsing support. Not yet integrated into NamedGraphSet's read methods

---
+++ src/de/fuberlin/wiwiss/ng4j/trig/TriGParser.java
@@ -0,0 +1,98 @@
+/*
+ * (c) Copyright 2001, 2002, 2003, Hewlett-Packard Development Company, LP
+ * [See end of file]
+ */
+
+package de.fuberlin.wiwiss.ng4j.trig;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+
+import antlr.TokenStreamException;
+import de.fuberlin.wiwiss.ng4j.trig.parser.TriGAntlrLexer;
+import de.fuberlin.wiwiss.ng4j.trig.parser.TriGAntlrParser;
+import de.fuberlin.wiwiss.ng4j.trig.parser.TriGAntlrParserTokenTypes;
+
+/** The formal interface to the TriG parser.  Wraps up the antlr parser and lexer.
+ * @author		Andy Seaborne
+ * @author Richard Cyganiak (richard@cyganiak.de)
+ * @version 	$Id: TriGParser.java,v 1.1 2004/11/22 00:46:19 cyganiak Exp $
+ */
+public class TriGParser implements TriGAntlrParserTokenTypes
+{
+	TriGAntlrLexer lexer = null ;
+	TriGAntlrParser parser = null ;
+	
+	public TriGParser(BufferedReader r, TriGParserEventHandler h)
+	{
+		lexer = new TriGAntlrLexer(r) ;
+		parser = new TriGAntlrParser(lexer) ;
+		parser.setEventHandler(h) ;
+		parser.setLexer(lexer) ;
+    }
+
+	public TriGParser(Reader r, TriGParserEventHandler h)
+	{
+		lexer = new TriGAntlrLexer(r) ;
+		parser = new TriGAntlrParser(lexer) ;
+		parser.setEventHandler(h) ;
+		parser.setLexer(lexer) ;
+    }
+    
+	public TriGParser(InputStream in, TriGParserEventHandler h)
+	{
+        Reader r = null ;
+        try { 
+            // UTF-8 always exists.
+            r = new InputStreamReader(in, "UTF-8") ;
+        } catch (UnsupportedEncodingException ex) {}
+        lexer = new TriGAntlrLexer(r) ;
+        parser = new TriGAntlrParser(lexer) ;
+		parser.setEventHandler(h) ;
+		parser.setLexer(lexer) ;
+    }
+
+    static public String[] getTokenNames() { return TriGAntlrParser._tokenNames ; }
+    
+    public int line() { return lexer.getLine() ; }
+    public int col() { return lexer.getColumn() ; }
+    
+    public TriGAntlrParser getParser()  { return parser ; }
+    public TriGAntlrLexer getLexer()    { return lexer ; }
+
+	/** Call the top level parser rule */
+	public void parse() throws antlr.RecognitionException, TokenStreamException
+	{
+		parser.document() ;
+	}
+}
+
+/*
+ *  (c) Copyright 2001, 2002, 2003 Hewlett-Packard Development Company, LP
+ *  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
---
+++ tests/de/fuberlin/wiwiss/ng4j/trig/TriGParserTest.java
@@ -0,0 +1,117 @@
+// $Id: TriGParserTest.java,v 1.1 2004/11/22 00:46:19 cyganiak Exp $
+package de.fuberlin.wiwiss.ng4j.trig;
+
+import java.io.Reader;
+import java.io.StringReader;
+
+import com.hp.hpl.jena.graph.Node;
+
+import junit.framework.TestCase;
+import de.fuberlin.wiwiss.ng4j.NamedGraphSet;
+import de.fuberlin.wiwiss.ng4j.Quad;
+import de.fuberlin.wiwiss.ng4j.impl.NamedGraphSetImpl;
+
+/**
+ * Tests for TriG parsing
+ *
+ * @author Richard Cyganiak (richard@cyganiak.de)
+ */
+public class TriGParserTest extends TestCase {
+	private static final String EX = "http://example.com/ex#";
+	private static final String BASE = "http://example.com/base";
+	private static final Node baseNode = Node.createURI(BASE);
+	private static final Node aNode = Node.createURI(EX + "a");
+	private static final Node bNode = Node.createURI(EX + "b");
+	private static final Node cNode = Node.createURI(EX + "c");
+	private static final Node graph1Node = Node.createURI(EX + "graph1");
+	private static final Node graph2Node = Node.createURI(EX + "graph2");
+	private static final Node graph3Node = Node.createURI(EX + "graph3");
+
+	private NamedGraphSet parseTriG(String triG) throws Exception {
+		Reader r = new StringReader(triG);
+		NamedGraphSetImpl ngs = new NamedGraphSetImpl();
+		NamedGraphSetPopulator h = 
+				new NamedGraphSetPopulator(ngs, BASE);
+		TriGParser p = new TriGParser(r, h);
+		p.parse();
+		return ngs;
+	}
+
+	public void testSimpleN3() throws Exception {
+		String n3 = "@prefix ex: <http://example.com/ex#> .\n" +
+				"ex:a ex:b ex:c .";
+		NamedGraphSet ngs = parseTriG(n3);
+		assertTrue(ngs.containsQuad(new Quad(baseNode, aNode, bNode, cNode)));
+		assertEquals(1, ngs.countQuads());
+	}
+	
+	public void testQuadFormula() throws Exception {
+		String triG = "@prefix ex: <http://example.com/ex#> .\n" +
+				"ex:graph1 :- { ex:a ex:b ex:c } .";
+		NamedGraphSet ngs = parseTriG(triG);
+		assertTrue(ngs.containsQuad(new Quad(graph1Node, aNode, bNode, cNode)));
+		assertEquals(1, ngs.countQuads());
+	}
+	
+	public void testIllegalNamingOperator() throws Exception {
+		String triG = "@prefix ex: <http://example.com/ex#> .\n" +
+		"ex:graph ex:foo { ex:a ex:b ex:c } .";
+		try {
+			parseTriG(triG);
+			fail();
+		} catch (TriGException ex) {
+			// is expected since ex:foo is not the graph naming operator :-
+			// TODO: Better error message
+		}
+	}
+	
+	public void testIllegalGraphName() throws Exception {
+		String triG = "@prefix ex: <http://example.com/ex#> .\n" +
+		"foo :- { ex:a ex:b ex:c } .";
+		try {
+			parseTriG(triG);
+			fail();
+		} catch (TriGException ex) {
+			// is expected since "foo" is not a legal graph name
+		}
+	}
+	
+	public void testNoNestedGraphs() throws Exception {
+		String triG = "@prefix ex: <http://example.com/ex#> .\n" +
+		"ex:graph :- { ex:graph :- { ex:a ex:b ex:c } } .";
+		try {
+			parseTriG(triG);
+			fail();
+		} catch (TriGException ex) {
+			// is expected because nesting graphs is not allowed
+		}		
+	}
+	
+	public void testMultipleGraphs() throws Exception {
+		String triG = "@prefix ex: <http://example.com/ex#> .\n" +
+				"ex:graph1 :- { ex:a ex:b ex:c } .\n" +
+				"ex:graph2 :- { ex:b ex:c ex:a } .\n" +
+				"ex:graph3 :- { ex:c ex:a ex:b } .\n";
+		NamedGraphSet ngs = parseTriG(triG);
+		assertTrue(ngs.containsQuad(new Quad(graph1Node, aNode, bNode, cNode)));
+		assertTrue(ngs.containsQuad(new Quad(graph2Node, bNode, cNode, aNode)));
+		assertTrue(ngs.containsQuad(new Quad(graph3Node, cNode, aNode, bNode)));
+		assertEquals(3, ngs.countQuads());
+	}	
+
+	public void testDefaultGraph() throws Exception {
+		String triG = "@prefix ex: <http://example.com/ex#> .\n" +
+				"ex:a ex:a ex:a .\n" +
+				"ex:graph1 :- { ex:a ex:b ex:c } .\n" +
+				"ex:b ex:b ex:b .\n" +
+				"ex:graph2 :- { ex:b ex:c ex:a } .\n" +
+				"ex:c ex:c ex:c .\n";
+		NamedGraphSet ngs = parseTriG(triG);
+		assertTrue(ngs.containsQuad(new Quad(graph1Node, aNode, bNode, cNode)));
+		assertTrue(ngs.containsQuad(new Quad(graph2Node, bNode, cNode, aNode)));
+		assertTrue(ngs.containsQuad(new Quad(baseNode, aNode, aNode, aNode)));
+		assertTrue(ngs.containsQuad(new Quad(baseNode, bNode, bNode, bNode)));
+		assertTrue(ngs.containsQuad(new Quad(baseNode, cNode, cNode, cNode)));
+		assertEquals(5, ngs.countQuads());
+	}
+}
