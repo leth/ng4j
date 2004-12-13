458f419efd452f0a74ba8178b46570edf0e21f9c
bizer <bizer>
2004-12-13 09:42:09 +0000

    Moved and changed interfaces.

---
+++ src/de/fuberlin/wiwiss/ng4j/swp/SWPAuthority.java
@@ -0,0 +1,136 @@
+/*
+ * Created on 24-Nov-2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package de.fuberlin.wiwiss.ng4j.swp;
+
+import java.util.ArrayList;
+
+import com.hp.hpl.jena.graph.Node;
+import com.hp.hpl.jena.graph.Graph;
+import com.hp.hpl.jena.util.iterator.ExtendedIterator;
+
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.cert.X509Certificate;
+
+import de.fuberlin.wiwiss.ng4j.NamedGraph;
+
+/**
+ * 
+ * An SWPAuthority represents information about an authorty
+ * like id, label, eMail, keys and certificates 
+ * 
+ * @author chris bizer
+ * @author rowland watkins
+ * 
+ */
+public interface SWPAuthority {
+
+    /**
+     * 
+     * Sets the ID of the authority.
+     * Authorities can by identified using a URIref or a bNode
+     * 
+     */
+	public void setID(Node id);
+	public Node getID();
+	
+	/**
+     * 
+     * Sets the Label / Name of the authority.
+     * Will be serialized using rdfs:label
+     * 
+     */
+	public void setLabel(String label);
+	public String getLabel();
+
+    /**
+     * 
+     * Sets the eMail adress of the authority.
+     * Will be serialized using foaf:mbox
+     * 
+     */
+	public void setEmail(String email);
+	public String getEmail();
+	
+    /**
+     * 
+     * Sets the public key of the authority.
+     * Will be serialized using swp:hasKey
+     * 
+     */
+	public void setPublicKey(PublicKey key);
+	public PublicKey getPublicKey();
+
+    /**
+     * 
+     * Sets the private key of the authority.
+     * 
+     */
+	public void setPrivateKey(PrivateKey key);
+	public PrivateKey getPrivateKey();	
+
+    /**
+     * 
+     * Sets the certificate of the authority.
+     * 
+     */
+	public void setCertificate(X509Certificate key);
+	public X509Certificate getCertificate();
+	
+    /**
+     * 
+     * Adds an additional property of the authority.
+     * 
+     */
+	public void addProperty(Node predicate, Node object);
+
+	/**
+     * 
+     * Returns an iterator over all property values (nodes) for a given property.
+     * 
+     */
+	public ExtendedIterator getProperty(Node predicate);
+
+    public boolean addDescriptionToGraph(NamedGraph graph, ArrayList listOfAuthorityProperties);
+
+	/**
+     * 
+     * Returns a graph containing all information about the authorty.
+     * Excluding its private key :-)
+     * 
+     */
+	public Graph getGraph();
+
+}
+
+/*
+ *  (c)   Copyright 2004 Chris Bizer (chris@bizer.de) & Rowland Watkins (rowland@grid.cx) 
+ *   	  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
---
+++ src/de/fuberlin/wiwiss/ng4j/swp/SWPNamedGraphSet.java
@@ -0,0 +1,266 @@
+/*
+ * Created on 24-Nov-2004
+ *
+ */
+package de.fuberlin.wiwiss.ng4j.swp;
+
+import de.fuberlin.wiwiss.ng4j.NamedGraphSet;
+
+import java.util.ArrayList;
+
+import com.hp.hpl.jena.graph.Node;
+import com.hp.hpl.jena.util.iterator.ExtendedIterator;
+
+/**
+ * 
+ * @author chris bizer
+ * @author rowland watkins
+ * 
+ */
+public interface SWPNamedGraphSet extends NamedGraphSet
+{
+    /**
+     * 
+     * Given an SWP Authority, assert all graphs in the 
+     * graphset with this Authority.
+     * 
+     * This will add a warrant graph asserting all
+     * other graphs to the graph set.
+     * 
+     * The listOfAuthorityProperties contains list of properties names
+     * (as nodes) describing the authority. These properties will be included
+     * into the warrant graph, e.g. foaf:name, foaf:mbox
+     * 
+     * Example:
+     * 
+     * urn:uuid:X { :G1 swp:assertedBy urn:uuid:X .
+	 *		        :G2 swp:assertedBy urn:uuid:X .
+     *              urn:uuid:X swp:assertedBy urn:uuid:X .
+     *              urn:uuid:X swp:authority <http://www.bizer.de/me> .
+     *              <http://www.bizer.de/me> foaf:mbox <mailto:chris@bizer.de> }
+     * 
+     * The new graph will be named using a UUID.
+     * 
+     * If the Authority doesn't have a URI, then a blank node will be used to
+     * identify the authority and a additional triple containing the foaf:mbox
+     * adress of the authority will be added.
+     *  
+     * Return true if successful.
+     * 
+     * @param authority 
+     * @param listOfAuthorityProperties
+     * @return
+     */
+    public boolean swpAssert( SWPAuthority authority, ArrayList listOfAuthorityProperties );
+
+    public boolean swpAssert(SWPAuthority authority);
+    
+    /**
+     * 
+     * Given an SWP Authority, quote all graphs in the 
+     * graphset with this Authority.
+     * 
+     * Quotes are not as strong semantically as assertions.
+     * Quotes are really used when using second hand 
+     * information, i.e. the Authority is not the creator
+     * of the original graph.
+     * 
+     * The listOfAuthorityProperties contains list of properties names
+     * (as nodes) describing the authority. These properties will be included
+     * into the warrant graph, e.g. foaf:name, foaf:mbox,
+     * 
+     * Example:
+     * 
+     * urn:uuid:X { :G1 swp:quotedBy urn:uuid:X .
+	 *		        :G2 swp:quotedBy urn:uuid:X .
+     *              urn:uuid:X swp:assertedBy urn:uuid:X .
+     *              urn:uuid:X swp:authority <http://www.bizer.de/me> .
+     *              <http://www.bizer.de/me> foaf:mbox <mailto:chris@bizer.de> }
+     *  
+     * Return true if successful.
+     * 
+     * @param authority
+     * @return
+     */
+    public boolean swpQuote( SWPAuthority authority, ArrayList listOfAuthorityProperties );
+
+	public boolean swpQuote(SWPAuthority authority);
+   
+    /**
+     * 
+     * Same as swpAssert, except instead of simply 
+     * asserting a graph, we sign the asserted
+     * graph with a digital signature according to
+     * the specified signatureMethod.
+     * 
+     * Example:
+     * 
+     * :Timestamp { :G1 swp:assertedBy :Timestamp .
+     *              :G1 swp:digestMethod swp:JjcRdfC14N-sha1 .
+     *              :G1 swp:digest "..." .
+	 *		        :G2 swp:assertedBy :Timestamp .
+     *              :G2 swp:digestMethod swp:JjcRdfC14N-sha1 .
+     *              :G2 swp:digest "..." .
+	 *		        :Timestamp swp:assertedBy :Timestamp .
+     *              :Timestamp swp:authority <http://www.bizer.de/me> .
+     *              :Timestamp swp:signatureMethod swp:JjcRdfC14N-rsa-sha1 .
+     *              :Timestamp swp:signature "..." } 
+     * 
+     * The listOfAuthorityProperties contains list of properties names
+     * (as nodes) describing the authority. These properties will be included
+     * into the warrant graph, e.g. foaf:name, foaf:mbox, swp:key, swp:certificate
+     * 
+     * Return true is successful.
+     * 
+     * @param authority
+     * @param signatureMethod
+     * @param digestMethod
+     * @param listOfAuthorityProperties
+     * @return
+     */
+    public boolean assertWithSignature( SWPAuthority authority, Node signatureMethod, Node digestMethod, ArrayList listOfAuthorityProperties );
+    
+    /**
+     * 
+     * Same as swpQuote, except instead of simply
+     * quoting a graph, we sign the quoted graph
+     * with a digital signature according to the
+     * specified signatureMethod.
+     * 
+     * Example:
+     * 
+     * :Timestamp { :G1 swp:quotedBy :Timestamp .
+     *              :G1 swp:digestMethod swp:JjcRdfC14N-sha1 .
+     *              :G1 swp:digest "..." .
+	 *		        :G2 swp:quotedBy :Timestamp .
+     *              :G2 swp:digestMethod swp:JjcRdfC14N-sha1 .
+     *              :G2 swp:digest "..." .
+	 *		        :Timestamp swp:assertedBy :Timestamp .
+     *              :Timestamp swp:authority <http://www.bizer.de/me> .
+     *              :Timestamp swp:signatureMethod swp:JjcRdfC14N-rsa-sha1 .
+     *              :Timestamp swp:signature "..." } 
+     * 
+     * The listOfAuthorityProperties contains list of properties names
+     * (as nodes) describing the authority. These properties will be included
+     * into the warrant graph, e.g. foaf:name, foaf:mbox, swp:key, swp:certificate
+     * 
+     * Return true of successful.
+     * 
+     * @param authority
+     * @param signatureMethod
+     * @param digestMethod
+     * @param listOfAuthorityProperties
+     * @return
+     */
+    public boolean quoteWithSignature( SWPAuthority authority, Node signatureMethod, Node digestMethod, ArrayList listOfAuthorityProperties );
+   
+    /**
+     * 
+     * Given an list of graph names and an SWP authority, assert
+     * each listed graph with this authority.
+     *
+     * Return true if successful.
+     * 
+     * @param listOfGraphnames as Nodes
+     * @param authority
+     * @param listOfAuthorityProperties
+     * @return
+     */
+    public boolean assertGraphs( ArrayList listOfGraphNames, SWPAuthority authority, ArrayList listOfAuthorityProperties );
+
+    public boolean quoteGraphs(ArrayList listOfGraphNames, SWPAuthority authority, ArrayList listOfAuthorityProperties);
+
+    /**
+     * 
+     * Given an list of graphs and an SWP Authority, assert
+     * each graph in the graphset with this Authority.
+     * 
+     * Return true if successful.
+     * 
+     * @param listOfGraphURIs
+     * @param authority
+     * @return
+     */
+    public boolean assertGraphsWithSignature( ArrayList listOfGraphNames, SWPAuthority authority, Node signatureMethod, Node digestMethod, ArrayList listOfAuthorityProperties );
+   
+    /**
+     * 
+     * For all signature graphs in the set,
+     * verify all signatures.
+     * 
+     * Calling this method requires adding a graph called 
+     * <http://localhost/trustedinformation> before.
+     * This graph has to contain the public keys and 
+     * certificates of authorities or root certificates by CAs
+     * trusted by the user. The content of this graph will we
+     * treated as trustworthy information in the signature 
+     * verification process.
+     * 
+     * The results of the signature verification process will be
+     * added as a new graph called <http://localhost/verifiedSignatures>
+     * to the graphset.
+     * 
+     * Example graph <http://localhost/verifiedSignatures>
+     * 
+     * <http://localhost/verifiedSignatures> { 
+     *              :Warrent1 swp:signatureVerification swp:sucessful .
+     *              :Warrent2 swp:signatureVerification swp:notSucessful .
+     *              :Warrent3 swp:signatureVerification swp:sucessful  } 
+     * 
+     * Return true if successful.
+     * 
+     * @return
+     */
+    public boolean verifyAllSignatures();
+    
+    
+	/**
+     * 
+     * Returns an iterator over all SWPWarrants for a given authority.
+     * 
+     */
+    public ExtendedIterator getAllWarrants( SWPAuthority authority );
+    
+	/**
+     * 
+     * Returns an iterator over all named graphs asserted by a given authority.
+     * 
+     */
+    public ExtendedIterator getAllAssertedGraphs( SWPAuthority authority );
+	/**
+     * 
+     * Returns an iterator over all named graphs quoted by a given authority.
+     * 
+     */
+    public ExtendedIterator getAllquotedGraphs( SWPAuthority authority );
+   
+
+}
+
+/*
+ *  (c)   Copyright 2004 Chris Bizer (chris@bizer.de) & Rowland Watkins (rowland@grid.cx) 
+ *   	  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
