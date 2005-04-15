07370ca4a8ae3c1f1f80500cd01ecf3e269c2482
maresch <maresch>
2005-04-15 11:42:05 +0000

    Added details property. The expl:details property contains ExplanationParts, which explain the parent ExplanationPath in more details.

--- src/de/fuberlin/wiwiss/trust/ExplanationPart.java
+++ src/de/fuberlin/wiwiss/trust/ExplanationPart.java
@@ -12,12 +12,14 @@ import com.hp.hpl.jena.graph.Triple;
 import com.hp.hpl.jena.vocabulary.RDF;
 
 /**
- * @version $Id: ExplanationPart.java,v 1.2 2005/03/22 01:01:48 cyganiak Exp $
+ * @version $Id: ExplanationPart.java,v 1.3 2005/04/15 11:42:05 maresch Exp $
  * @author Richard Cyganiak (richard@cyganiak.de)
+ * @author Oliver Maresch (oliver-maresch@gmx.de)
  */
 public class ExplanationPart {
     private Collection parts = new ArrayList();
     private List explanation;
+    private ExplanationPart details = null;
 
     public ExplanationPart() {
         this.explanation = Collections.EMPTY_LIST;
@@ -39,9 +41,16 @@ public class ExplanationPart {
         return this.explanation;
     }
     
-    public void writeAsRDF(Node parent, Graph target) {
+    public void setDetails(ExplanationPart details){
+        this.details = details;
+    }
+    
+    public ExplanationPart getDetails(){
+        return this.details;
+    }
+    
+    public Node writeAsRDF(Graph target) {
         Node thisPart = Node.createAnon();
-        target.add(new Triple(parent, EXPL.parts, thisPart));
         target.add(new Triple(thisPart, RDF.Nodes.type, EXPL.ExplanationPart));
         
         int counter = 1;
@@ -57,8 +66,16 @@ public class ExplanationPart {
         it = this.parts.iterator();
         while (it.hasNext()) {
             ExplanationPart part = (ExplanationPart) it.next();
-            part.writeAsRDF(thisPart, target);
+            Node child = part.writeAsRDF(target);
+            target.add(new Triple(thisPart, EXPL.parts, child));
         }
+        
+        if(details != null){
+            Node node = details.writeAsRDF(target);
+            target.add(new Triple(thisPart, EXPL.details, node));
+        }
+        
+        return thisPart;
     }
     
     public boolean equals(Object other) {
@@ -75,6 +92,10 @@ public class ExplanationPart {
         if (!otherExplanationPart.parts.containsAll(this.parts)) {
             return false;
         }
+        if(  (details != null && !details.equals(otherExplanationPart.details))
+          || (details == null && otherExplanationPart.details != null)){
+            return false;
+        }
         return true;
     }
     
@@ -86,17 +107,28 @@ public class ExplanationPart {
     
     public String toString() {
         StringBuffer result = new StringBuffer("Part" + this.explanation);
-        if (this.parts.isEmpty()) {
+        if (this.parts.isEmpty() && this.details == null) {
             return result.toString();
         }
         result.append(" <");
-        Iterator it = this.parts.iterator();
-        while (it.hasNext()) {
-            ExplanationPart child = (ExplanationPart) it.next();
-            result.append(child);
-            if (it.hasNext()) {
+        if(!this.parts.isEmpty()) {
+            result.append("Children(");
+            Iterator it = this.parts.iterator();
+            while (it.hasNext()) {
+                ExplanationPart child = (ExplanationPart) it.next();
+                result.append(child);
+                if (it.hasNext()) {
+                    result.append(", ");
+                }
+            }
+            result.append(")");
+            if(this.details != null){
                 result.append(", ");
             }
+        }
+        
+        if(details != null){
+            result.append("Details(" + details + ")");
         }
         result.append(">");
         return result.toString();
