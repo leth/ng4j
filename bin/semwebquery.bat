3885f0e6210f34307cb90ff6234c7441bc340386
tgauss <tgauss>
2006-10-09 15:29:32 +0000

    batch file fixed

--- bin/semwebquery.bat
+++ bin/semwebquery.bat
@@ -1,7 +1,7 @@
 @echo off
-@REM $Id: semwebquery.bat,v 1.1 2006/10/09 12:27:22 cyganiak Exp $
+@REM $Id: semwebquery.bat,v 1.2 2006/10/09 15:29:32 tgauss Exp $
 
-set NG4J_ROOT=%0/..
+set NG4J_ROOT=%0\..\..
 set CP=%NG4J_ROOT%\build;%NG4J_ROOT%\ng4j.jar
 pushd %NG4J_ROOT%
 for %%f in (%NG4J_ROOT%\lib\*.jar) do call :oneStep %%f
