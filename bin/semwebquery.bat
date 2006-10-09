abb868d2f0cb860313d4347d8b74a71a20ae5a67
cyganiak <cyganiak>
2006-10-09 15:46:29 +0000

    Generate ng4j.jar inside /lib instead of root

--- bin/semwebquery.bat
+++ bin/semwebquery.bat
@@ -1,8 +1,8 @@
 @echo off
-@REM $Id: semwebquery.bat,v 1.2 2006/10/09 15:29:32 tgauss Exp $
+@REM $Id: semwebquery.bat,v 1.3 2006/10/09 15:46:29 cyganiak Exp $
 
 set NG4J_ROOT=%0\..\..
-set CP=%NG4J_ROOT%\build;%NG4J_ROOT%\ng4j.jar
+set CP=%NG4J_ROOT%\build
 pushd %NG4J_ROOT%
 for %%f in (%NG4J_ROOT%\lib\*.jar) do call :oneStep %%f
 popd
