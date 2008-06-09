8d60445e45d3b81f50eadd6a24841234e714f6ef
hartig <hartig>
2008-06-09 19:42:59 +0000

    added the basics for a (Sindice-based) URI search

---
+++ src/de/fuberlin/wiwiss/ng4j/semwebclient/urisearch/URISearchListener.java
@@ -0,0 +1,11 @@
+package de.fuberlin.wiwiss.ng4j.semwebclient.urisearch;
+
+
+/**
+ * A listener that handles {@link URISearchResult}.
+ *
+ * @author Olaf Hartig
+ */
+public interface URISearchListener {
+	public void uriSearchFinished ( URISearchResult result );
+}
