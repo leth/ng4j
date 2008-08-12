930dafe608d0b26555e06b8799d8433da4c3021c
hartig <hartig>
2008-08-12 19:58:45 +0000

    Added the URI search to the query execution.

--- src/semweb/query.java
+++ src/semweb/query.java
@@ -25,7 +25,7 @@ import de.fuberlin.wiwiss.ng4j.semwebclient.CommandLineQuery;
  *       RDF/XML, N3, N-Triple, SPARQL XML results, SPARQL JSON results, CSV, ...
  * 
  * @author Richard Cyganiak (richard@cyganiak.de)
- * @version $Id: query.java,v 1.5 2008/06/09 16:19:04 hartig Exp $
+ * @version $Id: query.java,v 1.6 2008/08/12 19:58:45 hartig Exp $
  */
 public class query {
 	private static boolean verbose = false;
@@ -47,6 +47,7 @@ public class query {
 		cmd.add(false, "redirecteduris");
 		cmd.add(false, "verbose");
 		cmd.add(false, "grddl");
+		cmd.add(false, "urisearch");
 		try {
 			cmd.process(args);
 			if (!cmd.hasArgs() || cmd.hasItems()) {
@@ -106,6 +107,9 @@ public class query {
 		if (cmd.hasArg("grddl")) {
 			client.setEnableGrddl(true);
 		}
+		if (cmd.hasArg("urisearch")) {
+			client.setEnableURISearch(true);
+		}
 		if (cmd.hasArg("load")) {
 			Iterator it = cmd.getValues("load").iterator();
 			while (it.hasNext()) {
@@ -148,6 +152,7 @@ public class query {
 		System.out.println("    -timeout <seconds>     Set query timeout. Default: 60 seconds");
 		System.out.println("    -load <URL>            Load seed graph from the Web");
 		System.out.println("    -grddl                 Perform GRDDL transformations");
+		System.out.println("    -urisearch             Enables URI search during query execution");
 		System.out.println("    -loadtrig <file>       Load seed graphs from a TriG file before starting");
 		System.out.println("    -savetrig <file>       Save loaded graphs to a TriG file after finishing");
 		System.out.println("    -retrieveduris         Output a list of all successfully retrieved URIs");
