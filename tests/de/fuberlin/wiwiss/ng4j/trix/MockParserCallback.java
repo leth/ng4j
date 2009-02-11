deb62b6a8fdc5e5f158d2f6685f6f5b508df0dbd
jenpc <jenpc>
2009-02-11 01:55:26 +0000

    Added the type for many Collections and Iterators.  Added some comments.

--- tests/de/fuberlin/wiwiss/ng4j/trix/MockParserCallback.java
+++ tests/de/fuberlin/wiwiss/ng4j/trix/MockParserCallback.java
@@ -1,5 +1,5 @@
 /*
- * $Id: MockParserCallback.java,v 1.1 2004/10/23 13:31:23 cyganiak Exp $
+ * $Id: MockParserCallback.java,v 1.2 2009/02/11 01:55:26 jenpc Exp $
  */
 package de.fuberlin.wiwiss.ng4j.trix;
 
@@ -23,7 +23,7 @@ import junit.framework.Assert;
  * @author Richard Cyganiak (richard@cyganiak.de)
  */
 public class MockParserCallback implements ParserCallback {
-	private List expectedCalls = new LinkedList();
+	private List<String> expectedCalls = new LinkedList<String>();
 
 	public void addExpectation(String expectation) {
 		this.expectedCalls.add(expectation);
@@ -34,10 +34,10 @@ public class MockParserCallback implements ParserCallback {
 	}
 
 	public void expectStartGraph() {
-		expectStartGraph(new ArrayList(0));
+		expectStartGraph(new ArrayList<String>(0));
 	}
 
-	public void expectStartGraph(Collection uris) {
+	public void expectStartGraph(Collection<String> uris) {
 		addExpectation(getStartGraphCall(uris));
 	}
 
@@ -67,55 +67,88 @@ public class MockParserCallback implements ParserCallback {
 		}
 	}
 
-	public void startGraph(List uris) {
+	/* (non-Javadoc)
+	 * @see de.fuberlin.wiwiss.ng4j.trix.ParserCallback#startGraph(java.util.List)
+	 */
+	public void startGraph(List<String> uris) {
 		assertNext(getStartGraphCall(uris));
 	}
 
+	/* (non-Javadoc)
+	 * @see de.fuberlin.wiwiss.ng4j.trix.ParserCallback#endGraph()
+	 */
 	public void endGraph() {
 		assertNext("endGraph");
 	}
 
+	/* (non-Javadoc)
+	 * @see de.fuberlin.wiwiss.ng4j.trix.ParserCallback#subjectURI(java.lang.String)
+	 */
 	public void subjectURI(String uri) {
 		assertNext(getURICall(uri));
 	}
 
+	/* (non-Javadoc)
+	 * @see de.fuberlin.wiwiss.ng4j.trix.ParserCallback#subjectBNode(java.lang.String)
+	 */
 	public void subjectBNode(String id) {
 		assertNext(getIDCall(id));
 	}
 
+	/* (non-Javadoc)
+	 * @see de.fuberlin.wiwiss.ng4j.trix.ParserCallback#subjectPlainLiteral(java.lang.String, java.lang.String)
+	 */
 	public void subjectPlainLiteral(String value, String lang) {
 		assertNext(getPlainLiteralCall(value, lang));
 	}
 
+	/* (non-Javadoc)
+	 * @see de.fuberlin.wiwiss.ng4j.trix.ParserCallback#subjectTypedLiteral(java.lang.String, java.lang.String)
+	 */
 	public void subjectTypedLiteral(String value, String datatypeURI) {
 		assertNext(getTypedLiteralCall(value, datatypeURI));
 	}
 
+	/* (non-Javadoc)
+	 * @see de.fuberlin.wiwiss.ng4j.trix.ParserCallback#predicate(java.lang.String)
+	 */
 	public void predicate(String uri) {
 		assertNext(getURICall(uri));
 	}
 
+	/* (non-Javadoc)
+	 * @see de.fuberlin.wiwiss.ng4j.trix.ParserCallback#objectURI(java.lang.String)
+	 */
 	public void objectURI(String uri) {
 		assertNext(getURICall(uri));
 	}
 
+	/* (non-Javadoc)
+	 * @see de.fuberlin.wiwiss.ng4j.trix.ParserCallback#objectBNode(java.lang.String)
+	 */
 	public void objectBNode(String id) {
 		assertNext(getIDCall(id));
 	}
 
+	/* (non-Javadoc)
+	 * @see de.fuberlin.wiwiss.ng4j.trix.ParserCallback#objectPlainLiteral(java.lang.String, java.lang.String)
+	 */
 	public void objectPlainLiteral(String value, String lang) {
 		assertNext(getPlainLiteralCall(value, lang));
 	}
 
+	/* (non-Javadoc)
+	 * @see de.fuberlin.wiwiss.ng4j.trix.ParserCallback#objectTypedLiteral(java.lang.String, java.lang.String)
+	 */
 	public void objectTypedLiteral(String value, String datatypeURI) {
 		assertNext(getTypedLiteralCall(value, datatypeURI));
 	}
 
-	private String getStartGraphCall(Collection uris) {
-		List sortedURIs = new ArrayList(uris);
+	private String getStartGraphCall(Collection<String> uris) {
+		List<String> sortedURIs = new ArrayList<String>(uris);
 		Collections.sort(sortedURIs);
 		StringBuffer call = new StringBuffer("startGraph");
-		Iterator it = sortedURIs.iterator();
+		Iterator<String> it = sortedURIs.iterator();
 		if (it.hasNext()) {
 			call.append(":");
 		}
@@ -156,3 +189,31 @@ public class MockParserCallback implements ParserCallback {
 		Assert.assertEquals(this.expectedCalls.remove(0), actual);
 	}
 }
+
+/*
+ *  (c) Copyright 2004, 2005, 2006, 2007, 2008 Christian Bizer (chris@bizer.de)
+ *   All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
