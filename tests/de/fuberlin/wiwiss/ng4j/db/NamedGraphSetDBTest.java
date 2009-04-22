13cd517ac4e2fe854bbc9234c6cbda2e660b3278
jenpc <jenpc>
2009-04-22 17:25:35 +0000

    Added some javadoc, annotations, and other comments.

--- tests/de/fuberlin/wiwiss/ng4j/db/NamedGraphSetDBTest.java
+++ tests/de/fuberlin/wiwiss/ng4j/db/NamedGraphSetDBTest.java
@@ -1,4 +1,4 @@
-// $Id: NamedGraphSetDBTest.java,v 1.5 2009/02/15 02:12:56 jenpc Exp $
+// $Id: NamedGraphSetDBTest.java,v 1.6 2009/04/22 17:25:35 jenpc Exp $
 package de.fuberlin.wiwiss.ng4j.db;
 
 import java.util.ArrayList;
@@ -27,10 +27,18 @@ public class NamedGraphSetDBTest extends NamedGraphSetTest {
 
 	public static final String NL = System.getProperty("line.separator") ;
 	
+	/* (non-Javadoc)
+	 * @see de.fuberlin.wiwiss.ng4j.NamedGraphSetTest#createNamedGraphSet()
+	 */
+	@Override
 	public NamedGraphSet createNamedGraphSet() throws Exception {
 		return DBConnectionHelper.createNamedGraphSetDB();
 	}
 
+	/* (non-Javadoc)
+	 * @see de.fuberlin.wiwiss.ng4j.NamedGraphSetTest#tearDown()
+	 */
+	@Override
 	public void tearDown() throws Exception {
 		super.tearDown();
 		DBConnectionHelper.deleteNamedGraphSetTables();
@@ -42,6 +50,10 @@ public class NamedGraphSetDBTest extends NamedGraphSetTest {
 		assertFalse(this.set.isEmpty());
 	}
 	
+	/* (non-Javadoc)
+	 * @see de.fuberlin.wiwiss.ng4j.NamedGraphSetTest#testAddGraph()
+	 */
+	@Override
 	public void testAddGraph() {
 		NamedGraph ng = new NamedGraphImpl(node1, new GraphMem());
 		assertFalse(this.set.containsGraph(node1));
