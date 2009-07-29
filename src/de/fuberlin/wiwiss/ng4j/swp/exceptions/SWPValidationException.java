088c094102582d9e8456c7c602b564a8d95ed07d
timp <timp>
2009-07-29 11:48:25 +0000

    Remove TODO

--- src/de/fuberlin/wiwiss/ng4j/swp/exceptions/SWPValidationException.java
+++ src/de/fuberlin/wiwiss/ng4j/swp/exceptions/SWPValidationException.java
@@ -1,13 +1,8 @@
-/*
- * Created on 11-Dec-2004
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 package de.fuberlin.wiwiss.ng4j.swp.exceptions;
 
 /**
  * @author rowland
+ * @since 11-Dec-2004
  *
  * Declarative Systems & Software Engineering Group,
  * School of Electronics & Computer Science,
