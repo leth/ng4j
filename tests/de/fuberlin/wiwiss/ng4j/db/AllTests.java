adf0484a254475c2d46f44858fb08f61622d0bd2
jenpc <jenpc>
2010-09-16 13:38:18 +0000

    Updated comments/javadoc.  No code changes.

--- tests/de/fuberlin/wiwiss/ng4j/db/AllTests.java
+++ tests/de/fuberlin/wiwiss/ng4j/db/AllTests.java
@@ -1,14 +1,15 @@
-// $Id: AllTests.java,v 1.3 2009/01/21 01:37:39 jenpc Exp $
+// $Id: AllTests.java,v 1.4 2010/09/16 13:38:18 jenpc Exp $
 package de.fuberlin.wiwiss.ng4j.db;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
 /**
- * Test suite for NamedGraphSetDB and its helper classes. Needs a MySQL database.
- * Connection data must be provided inside this file.
- *
- * TODO: Database access data shouldn't be here
+ * Test suite for NamedGraphSetDB and its helper classes. 
+ * Connection data must be provided inside class DBConnectionHelper.
+ * Defaults to in-memory HSQLDB, but can be changed to another
+ * database such as PostgreSQL or MySQL.  If changed, do so only locally,
+ * and include the appropriate database driver as a library on the .classpath.
  * 
  * @author Richard Cyganiak (richard@cyganiak.de)
  */
